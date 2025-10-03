71182a26adbcfc2d1b7adc913632bc2001f7d791
drazzilb1424 <32851052+drazzilb1424@users.noreply.github.com>
2025-10-03 13:39:31 -0400

    Add files via upload

---
+++ search.js
@@ -0,0 +1,58 @@
+function searchPSAOrder() {
+  const input = document.getElementById("psa-lookup").value.trim();
+
+  const orderPages = {
+    "018": "/submissions/psa-tracker-order-018/",
+    "161": "/submissions/psa-tracker-order-161.html",
+    "202": "/submissions/psa-tracker-order-202.html",
+    "240": "/submissions/psa-tracker-order-240.html",
+    "257": "/submissions/psa-tracker-order-257.html",
+    "258": "/submissions/psa-tracker-order-258.html",
+    "278": "/submissions/psa-tracker-order-278.html",
+    "385": "/submissions/psa-tracker-order-385.html",
+    "418": "/submissions/psa-tracker-order-418.html",
+    "463": "/submissions/psa-tracker-order-463.html",
+    "484": "/submissions/psa-tracker-order-484.html",
+    "538": "/submissions/psa-tracker-order-538.html",
+    "581": "/submissions/psa-tracker-order-581.html",
+    "695": "/submissions/psa-tracker-order-695.html",
+    "701": "/submissions/psa-tracker-order-701.html",
+    "757": "/submissions/psa-tracker-order-757.html",
+    "781": "/submissions/psa-tracker-order-781.html",
+    "878": "/submissions/psa-tracker-order-878.html",
+    "847": "/submissions/psa-tracker-order-847.html",
+    "854": "/submissions/psa-tracker-order-854.html",
+    "382": "/submissions/psa-tracker-order-382.html",
+    "337": "/submissions/psa-tracker-order-337.html",
+    "241": "/submissions/psa-tracker-order-241.html",
+    "170": "/submissions/psa-tracker-order-170.html",
+    "008": "/submissions/psa-tracker-order-008.html",
+    "213": "/submissions/psa-tracker-order-213.html",
+    "311": "/submissions/psa-tracker-order-311.html",
+    "449": "/submissions/psa-tracker-order-449.html",
+    "852": "/submissions/psa-tracker-order-852.html",
+    "902": "/submissions/psa-tracker-order-902.html",
+  };
+
+  if (orderPages[input]) {
+    window.location.href = orderPages[input];
+  } else {
+    window.location.href = "/submission-not-found.html"; // Redirect to not found page
+  }
+}
+
+function searchPSACert() {
+  const certNumber = document.getElementById("psa-cert-lookup").value.trim();
+  if (certNumber) {
+    // Example: redirect to your cert lookup page with cert as query param
+    window.location.href = `/psa-cert.html?cert=${certNumber}`;
+  } else {
+    alert("Please enter a valid cert number.");
+  }
+}
+
+function toggleMenu() {
+  const menu = document.getElementById('mobile-menu');
+  menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
+
+}
